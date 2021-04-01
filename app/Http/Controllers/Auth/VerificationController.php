90d10e4cd9d8385df422e46a90b8f981a9bfc1ae
HYH <h2005yh@126.com>
2021-04-01 11:27:36 +0000

    认证后的消息提醒

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -26,8 +26,9 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+//    protected $redirectTo = RouteServiceProvider::HOME;
 
+    protected $redirectTo = '/';
     /**
      * Create a new controller instance.
      *
