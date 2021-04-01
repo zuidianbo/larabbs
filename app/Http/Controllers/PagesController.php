79949830663bb0334c37834d9dd1982cf7e92a68
HYH <h2005yh@126.com>
2021-04-01 11:00:15 +0000

    邮箱认证

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,8 +6,18 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
+
+//    首页
     public function root()
     {
+
+//        测试 是否已经验证了邮箱
+
+//        dd(\Auth::user()->hasVerifiedEmail());
         return view('pages.root');
+
     }
+
+
+
 }
