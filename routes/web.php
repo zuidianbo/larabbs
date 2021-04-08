4fb4cb7ec8d3f88857fd16646657d5d7480149a4
HYH <h2005yh@126.com>
2021-04-08 02:42:05 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -53,3 +53,4 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 
 
 //--------------------
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
