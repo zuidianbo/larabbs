4fd6f3d1e00326f2675f392efbffccb9f3ac8604
HYH <h2005yh@126.com>
2021-04-09 01:18:08 +0000

    用户可以创建话题

--- app/helpers.php
+++ app/helpers.php
@@ -10,4 +10,12 @@ function category_nav_active($category_id)
     return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
 }
 
+
+//创建帖子保存的时候，生成excerpt字段的数据
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return Str::limit($excerpt, $length);
+}
+
 ?>
