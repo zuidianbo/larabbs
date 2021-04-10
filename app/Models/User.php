1655e947b824cf81afaa94fd5891b62d91f4b328
HYH <h2005yh@126.com>
2021-04-10 01:08:58 +0000

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -87,4 +87,12 @@ class User extends Authenticatable implements MustVerifyEmailContract
         return $this->hasMany(Reply::class);
     }
 
+//    清空消息
+
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -74,3 +74,7 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 
 //Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+
+//通知列表
+
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
