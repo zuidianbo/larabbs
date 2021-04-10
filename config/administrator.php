fd84a365319e9758543c039f3c0656c55f49a6d4
HYH <h2005yh@126.com>
2021-04-10 07:05:11 +0000

    内容管理后台

--- config/administrator.php
+++ config/administrator.php
@@ -182,12 +182,16 @@ return array(
 //        ],
 //    ],
 
-
     'menu' => [
         '用户与权限' => [
             'users',
             'roles',
             'permissions',
+        ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
         ],
     ],
 
