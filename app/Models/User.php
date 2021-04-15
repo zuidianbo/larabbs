3406b6359cc2d7d249d34c12457cc2dae5fe6176
HYH <h2005yh@126.com>
2021-04-15 02:19:23 +0000

    微信登录

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,7 +56,8 @@ class User extends Authenticatable implements MustVerifyEmailContract
 
 
     protected $fillable = [
-        'name', 'phone', 'email', 'password','introduction','avatar'
+        'name', 'phone', 'email', 'password', 'introduction', 'avatar',
+        'weixin_openid', 'weixin_unionid'
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -78,6 +78,14 @@ Route::prefix('v1')
             });
 
 
+        // 用户注册
+        Route::post('users', 'UsersController@store')
+            ->name('users.store');
+        // 第三方登录
+        Route::post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+            ->where('social_type', 'weixin')
+            ->name('socials.authorizations.store');
+
 
 
 
